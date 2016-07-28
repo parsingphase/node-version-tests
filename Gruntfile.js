30e34a6db50597324259830a4af21179f8159cbc
Richard George <r.george@quidco.com>
2016-07-28 10:30:18 +0100

    Revise imagemin version

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,6 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-
         imagemin: {
             pix: {
                 files: [
