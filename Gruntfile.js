d53cefdc41947fd47fdd2d1f98a9fb9ed381b02f
Richard George <r.george@quidco.com>
2016-07-28 09:46:40 +0100

    Multi-node travis test

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+    // all paths relative to directory of this file
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        imagemin: {
+            pix: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['imgsrc/*.{png,jpg,gif,svg}'],
+                        dest: 'built/imgdst/'
+                    }
+                ]
+            }
+        },
+        clean: {
+            built: [
+                // individual clean tasks can't assume these are completely un-needed
+                'built'
+            ]
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-bless');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // ///////////// v3 tasks ///////////////
+
+    grunt.registerTask(
+        'default',
+        'Test build',
+        ['imagemin:pix']
+    );
+};
